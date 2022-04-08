7db5922c8f540b9a326686420ed4cdff46bf149e
AlecDong <101013007+AlecDong@users.noreply.github.com>
2022-04-07 20:34:02 -0400

    front end changed to use FuncAnimation

--- Front_end.py
+++ Front_end.py
@@ -1,13 +1,55 @@
 from tkinter import *
-from matplotlib.figure import Figure
-from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg)
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib import pyplot as plt, animation
 import csv
 from PIL import ImageTk,Image
 
-index = 0
+# Global vars
 threshold = 2
+img = None
 
+# Create tkinter window
+root = Tk()
+root.title('Riverdale Library Air Exchange Rate')
+screen_width = int(root.winfo_screenwidth() / 2)
+screen_height = int(root.winfo_screenheight() / 3 * 2)
 
+# Labels for headings
+lbl_intro = Label(root, justify="left", text="Riverdale Library Air Exchange Rate", font=("Arial", 25)).grid(column=0, row=0)
+lbl_safe = Label(root, justify="right", text="What is safe?", font=("Arial", 25)).grid(column=1, row=0)
+
+# Labels for descriptions
+lbl_intro_desc = Label(root, justify="center", wraplength=500, text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum").grid(column=0, row=1)
+lbl_safe_desc = Label(root, justify="center", wraplength=500, text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum").grid(column=1, row=1)
+
+# The canvas for the image
+image_canvas = Canvas(root, width=min(screen_width, screen_height), height=min(screen_width, screen_height))
+image_canvas.grid(row = 2, column = 0)
+
+# Setting up the graph
+fig = plt.Figure(figsize=(min(screen_width, screen_height) / 100, min(screen_width, screen_height) / 100), dpi=100)
+ax = fig.add_subplot(xlim=(0, 60), ylim=(-55, 55))
+ax.set_title('Riverdale Air Exchange Rate')
+ax.set_xlabel('Last 60 minutes')
+ax.set_ylabel('Air changes per hour')
+
+# Adding lines to the graph
+threshold_line, = ax.plot([], [], ':', lw=2, label="Threshold")
+line, = ax.plot([], [], lw=2, label="Unit 1")
+line2, = ax.plot([], [], lw=2, label="Unit 2")
+line3, = ax.plot([], [], lw=2, label="Unit 3")
+line4, = ax.plot([], [], lw=2, label="Unit 4")
+line5, = ax.plot([], [], lw=2, label="Unit 5")
+
+# Set legend
+ax.legend()
+
+# The canvas for the graph
+canvas = FigureCanvasTkAgg(fig, master=root)
+canvas.draw()
+canvas.get_tk_widget().grid(row = 2, column = 1)
+
+# Opens a csv file into a list
 def read_csv(file_name, index):
     y_vals = []
     with open(file_name, newline="") as file:
@@ -16,115 +58,54 @@ def read_csv(file_name, index):
             num = row[0]
             num = round(float(num),2)
             y_vals.append(num)
-            # if len(y_vals) > 60:
-            #     y_vals.pop(0)
     return y_vals[index : index+60]
 
+# Animates the graph
+def animate(i):
+    global threshold
 
-def is_safe(values, threshold):
+    # Reads data
+    x = [j for j in range(60)]
+    y = read_csv("data1.csv", i)
+    y2 = read_csv("data2.csv", i)
+    y3 = read_csv("data3.csv", i)
+    y4 = read_csv("data4.csv", i)
+    y5 = read_csv("data5.csv", i)
+
+    # Updates the lines
+    threshold_line.set_data(x, [threshold for _ in range(60)])
+    line.set_data(x, y)
+    line2.set_data(x, y2)
+    line3.set_data(x, y3)
+    line4.set_data(x, y4)
+    line5.set_data(x, y5)
+
+    # Determine which picture to load
+    global img
+    values = y + y2 + y3 + y4 + y5
+    if is_safe(values):
+        img = Image.open("thumbsup.jpg")
+    else:
+        img = Image.open("exclaim.jpg")
+
+    resized_image = img.resize((min(screen_width, screen_height),min(screen_width, screen_height)))
+
+    img = ImageTk.PhotoImage(resized_image)
+
+    # Load the image
+    image_canvas.create_image(0,0, image = img, anchor="nw")
+
+    return line, line2, line3, line4, line5,
+
+# Check if values are above the threshold
+def is_safe(values):
+    global threshold
     for value in values:
         if value < threshold:
             return False
     return True
 
+# Graph animation
+anim = animation.FuncAnimation(fig, animate, interval=20, blit=True)
 
-# plot function is created for
-# plotting the graph in
-# tkinter window
-def plot():
-    global index
-    global threshold
-    # air exchange rates from each unit
-    y = read_csv("data1.csv", index)
-    y2 = read_csv("data2.csv", index)
-    y3 = read_csv("data3.csv", index)
-    y4 = read_csv("data4.csv", index)
-    y5 = read_csv("data5.csv", index)
-
-    # the figure that will contain the plot
-    fig = Figure(figsize = (8, 5), dpi = 100)
-
-
-    # adding the subplot
-    plot1 = fig.add_subplot(111) 
-
-    # plotting the graph
-    plot1.set_title("Riverdale Library Air Exchange")
-    plot1.set(xlabel="Last 60 Minutes", ylabel="Air Changes per Hour")
-    plot1.plot(y, label="Unit 1")
-    plot1.plot(y2, label="Unit 2")
-    plot1.plot(y3, label="Unit 3")
-    plot1.plot(y4, label="Unit 4")
-    plot1.plot(y5, label="Unit 5")
-    plot1.plot([threshold for _ in range(60)],':', label="Threshold")
-    plot1.legend()
-
-    for widgets in frame.winfo_children():
-        widgets.destroy()
-
-    # creating the Tkinter canvas
-    # containing the Matplotlib figure
-    canvas = FigureCanvasTkAgg(fig, master = frame)
-    canvas.draw()
-
-    # placing the canvas on the Tkinter window
-    canvas.get_tk_widget().grid()
-
-    values = y + y2 + y3 + y4 + y5
-    
-    global img
-
-    if is_safe(values, threshold):
-        img = Image.open("smile.png")
-    else:
-        img = Image.open("exclaim.png")
-
-    resized_image = img.resize((500,500), Image.ANTIALIAS)
-
-    img = ImageTk.PhotoImage(resized_image)
-
-    canvas2.create_image(0,0, image = img, anchor="nw")
-
-    index += 1
-    
-    window.after(1000, plot)
-
-# the main Tkinter window
-window = Tk()
-screen_width = window.winfo_screenwidth()
-screen_height = window.winfo_screenheight()
-
-# setting the title
-window.title('Riverdale Air Exchange Rate')
-
-# dimensions of the main window
-# window.geometry(str(screen_width)+"x"+str(screen_height))
-
-introduction = Frame(window)
-introduction.grid(row=1, columnspan=3)
-
-lbl_intro = Label(introduction, justify="left", text="Riverdale Library Air Exchange Rate", font=("Arial", 25)).grid(column=0, row=1)
-lbl_safe = Label(introduction, justify="right", text="What is safe?", font=("Arial", 25)).grid(column=2, row=1)
-
-lbl_intro_desc = Label(introduction, justify="left", wraplength=500, text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum").grid(column=0, row=2)
-lbl_safe_desc = Label(introduction, justify="right", wraplength=500, text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum").grid(column=2, row=2)
-
-frame1 = Frame(window)
-frame1.grid(row=2, column=1)
-
-frame = Frame(window)
-frame.grid(row=2, column=2)
-
-# image beside the plot
-canvas2 = Canvas(frame1, width=500, height=500)
-
-# place the image
-# in the frame
-canvas2.grid()
-
-img = None
-
-plot()
-
-# run the gui
-window.mainloop()
+mainloop()
